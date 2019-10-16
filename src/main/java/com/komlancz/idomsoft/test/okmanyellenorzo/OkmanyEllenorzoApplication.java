9fc7726fb7ff73ee7c14b103cbd0390ac2265fed
Komlancz Janos <jkomlancz@gmail.com>
2019-10-16 18:45:11 +0200

    initial commit

---
+++ src/main/java/com/komlancz/idomsoft/test/okmanyellenorzo/OkmanyEllenorzoApplication.java
@@ -0,0 +1,13 @@
+package com.komlancz.idomsoft.test.okmanyellenorzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkmanyEllenorzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkmanyEllenorzoApplication.class, args);
+	}
+
+}
