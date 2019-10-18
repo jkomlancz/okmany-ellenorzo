6946b84e3ddf6e0f97dda98b0d914a4028f607aa
Komlancz Janos <jkomlancz@gmail.com>
2019-10-18 18:04:26 +0200

    be able to run on prdod tomcat

--- src/main/java/com/komlancz/idomsoft/test/okmanyellenorzo/OkmanyEllenorzoApplication.java
+++ src/main/java/com/komlancz/idomsoft/test/okmanyellenorzo/OkmanyEllenorzoApplication.java
@@ -2,9 +2,16 @@ package com.komlancz.idomsoft.test.okmanyellenorzo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OkmanyEllenorzoApplication {
+public class OkmanyEllenorzoApplication extends SpringBootServletInitializer {
+
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(OkmanyEllenorzoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OkmanyEllenorzoApplication.class, args);
--- src/main/java/com/komlancz/idomsoft/test/okmanyellenorzo/kontroller/OkmanyEllenorzoKotroller.java
+++ src/main/java/com/komlancz/idomsoft/test/okmanyellenorzo/kontroller/OkmanyEllenorzoKotroller.java
@@ -5,9 +5,7 @@ import com.komlancz.idomsoft.test.okmanyellenorzo.model.Valasz;
 import com.komlancz.idomsoft.test.okmanyellenorzo.segito.ValidatorBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,6 +15,19 @@ public class OkmanyEllenorzoKotroller {
 
     @Autowired
     ValidatorBean validatorBean;
+
+    @GetMapping(path = "/")
+    public String welcome(){
+        return "POST hívással lehet használni az alkalmazást az alábbi path-on: /okmany-ellenorzes";
+    }
+
+    @GetMapping(path = "/{p}")
+    public String welcome2(@PathVariable("p") String path){
+        String rosszPath = String.format("/%s GET nem elérhető! ", path);
+        return rosszPath.isEmpty() ?
+                "POST hívással lehet használni az alkalmazást az alábbi path-on: /okmany-ellenorzes"
+                : rosszPath + "POST hívással lehet használni az alkalmazást az alábbi path-on: /okmany-ellenorzes";
+    }
 
     @PostMapping(path = "/okmany-ellenorzes")
     public Valasz okmanyokEllenorzese(@RequestBody List<OkmanyDTO> okmanyok){
